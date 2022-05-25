79eb3c06437c18b66c5a19f35623fff289fc85ee
Daniel Lundy <daniel.lundy@dcmail.ca>
2022-05-25 10:11:55 -0400

    Set up the book skeleton.

---
+++ js/script.js
@@ -0,0 +1,6 @@
+console.log("connected");
+console.log("%c Sweet Mother, sweet Mother, send your child unto me, for the sins of the unworthy must be baptized in blood and fear...", "color: red");
+
+// Flip animation function
+
+$('#bookOne').turn({gradients: true, acceleration: true});
